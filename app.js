b7977a811e17e50d0b3842318ca2a7aff4e34b54
danielpurnomo <76069399+danielpurnomo@users.noreply.github.com>
2024-08-03 22:32:32 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('refreshButton').addEventListener('click', function() {
+        window.location.reload();
+    });
+});
